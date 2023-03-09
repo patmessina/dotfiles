7f644b95c86d77d71abfc54880c941089d100c16
Patrick Messina <patrick@messinamail.com>
2023-03-09 00:53:57 -0500

    added aliases and color

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,23 @@
 # general
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
 alias ls="ls --color"
+alias ll="ls -l"
+
+alias rm="rm -I"
+alias cp="cp -i"
+alias mkdir="mkdir -p"
+
+alias c="clear"
+
+alias grep='grep --color=auto'
+alias ip='ip -color=auto'
+
+alias df="df -h"
+alias du="du -h"
 
 # git
 #
@@ -12,5 +30,3 @@ alias gcs="git checkout stage"
 alias gc="git checkout"
 alias gcmsg="git commit -m"
 alias gp="git push"
-
-
