f660b43d3c3f74378a408f07e447acf0b2bbf7b3
Patrick Messina <patrick@messinamail.com>
2023-04-22 23:27:46 -0400

    can now handle nixos for fzf

--- zsh/fzf/linux-fzf.zsh
+++ zsh/fzf/linux-fzf.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/env zsh
 
-source /usr/share/fzf/key-bindings.zsh
-source /usr/share/fzf/completion.zsh
+if command -v fzf-share > /dev/null; then
+  source "$(fzf-share)/key-bindings.zsh"
+  source "$(fzf-share)/completion.zsh"
+else
+  source /usr/share/fzf/key-bindings.zsh
+  source /usr/share/fzf/completion.zsh
+fi
