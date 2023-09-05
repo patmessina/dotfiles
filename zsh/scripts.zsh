9c1382f24dba72b260ca78c5bffbd87ddc779777
Patrick Messina <patrick_messina@eogresources.com>
2023-09-05 09:50:43 -0500

    added add ssh keys

--- zsh/scripts.zsh
+++ zsh/scripts.zsh
@@ -5,3 +5,5 @@ source ${DOTFILES}/zsh/scripts/backup.zsh
 source ${DOTFILES}/zsh/scripts/calc.zsh
 source ${DOTFILES}/zsh/scripts/cheat.zsh
 source ${DOTFILES}/zsh/scripts/reset-completions.zsh
+source ${DOTFILES}/zsh/scripts/reset-completions.zsh
+source ${DOTFILES}/zsh/scripts/scprc.zsh
